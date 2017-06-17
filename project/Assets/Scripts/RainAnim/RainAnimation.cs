3703c88a0184a36683e01708dabfed4156b0a1dd
TSITCL\s162133 <trident0310@gmail.com>
2017-06-17 15:35:57 +0900

    雨作成

---
+++ project/Assets/Scripts/RainAnim/RainAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainAnimation : MonoBehaviour {
+    Animator animator;
+    int raincnt;
+    bool rainflag;
+    // Use this for initialization
+    void Start () {
+        this.animator = GetComponent<Animator>();
+        rainflag = false;
+        //表示する
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+
+        Vector3 position = Input.mousePosition;
+        position.z = 10f;
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        Vector3 screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        // ワールド座標に変換されたマウス座標を代入
+        gameObject.transform.position = screenToWorldPointPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+  
+
+       
+            raincnt++;
+            if (raincnt > 15)
+            {
+                Destroy(gameObject);
+            }
+             
+        
+    }
+}
