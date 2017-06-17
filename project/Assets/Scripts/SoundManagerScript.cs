2df4874c3a80516c9668d6a34a98d156ade67455
yosukeadachi <yosukeadachi@gmail.com>
2017-06-17 17:01:23 +0900

    UI調整

--- project/Assets/Scripts/SoundManagerScript.cs
+++ project/Assets/Scripts/SoundManagerScript.cs
@@ -11,7 +11,6 @@ public class SoundManagerScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		sources = gameObject.GetComponents<AudioSource>();
-        playBgmRain();
 	}
 	
 	// Update is called once per frame
