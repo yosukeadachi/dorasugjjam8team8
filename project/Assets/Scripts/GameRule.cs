3017107cbb7ee3792cd3b88b5853f732220f51c1
yosukeadachi <yosukeadachi@gmail.com>
2017-06-17 14:07:42 +0900

    ゲームの流れを追加

---
+++ project/Assets/Scripts/GameRule.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameRule : MonoBehaviour {
+	//インスペクターから設定
+	public Text startText;
+	public Text successText;
+	public Text failText;
+	private GameObject butoonToTitle;
+
+	//フロー設定
+	public enum Flow {
+		START,
+		PLAYING,
+		SUCCESS,
+		FAIL,
+	}
+
+	public Flow nowFlow;
+
+	private float startTimeCount;
+
+	// Use this for initialization
+	void Start () {
+		startTimeCount = 1.0f;
+		nowFlow = Flow.START;
+		startText.enabled = true;
+		successText.enabled = false;
+		failText.enabled = false;
+		butoonToTitle = GameObject.Find("ButtonToTitle");
+		butoonToTitle.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(nowFlow) {
+			case Flow.START:
+				flowStart();
+				break;
+			case Flow.PLAYING:
+				flowPlaying();
+				break;
+			case Flow.SUCCESS:
+				flowSuccess();
+				break;
+			case Flow.FAIL:
+				flowFail();
+				break;
+			default:
+				break;
+		}
+	}
+
+	public void toFlowFail() {
+		Debug.Log("toFlowFail");
+		nowFlow = Flow.FAIL;
+		failText.enabled = true;
+		butoonToTitle.SetActive(true);
+	}
+
+	public void toFlowSuccess() {
+		nowFlow = Flow.SUCCESS;
+		successText.enabled = true;
+		butoonToTitle.SetActive(true);
+	}
+
+
+	void flowStart() {
+		startTimeCount -= Time.deltaTime;
+		// Debug.Log(startTimeCount);
+		if(startTimeCount < 0.0f) {
+			nowFlow = Flow.PLAYING;
+			startText.enabled = false;
+		}
+	}
+
+	void flowPlaying() {
+		//タグで透明人間検索
+		//全員死んでたら
+		//toFlowSuccess();
+	}
+
+	void flowSuccess() {
+
+	}
+
+	void flowFail() {
+
+	}
+
+}
---
+++ project/Assets/Scripts/GameTimerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimerScript : MonoBehaviour {
+	public GameRule gaameRule;
+	public Text leftTimeText;
+
+	private float countTime;
+
+	// Use this for initialization
+	void Start () {
+		countTime = 10.0f;
+		leftTimeText.text = countTime.ToString("F2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (gaameRule.nowFlow)
+		{
+			case GameRule.Flow.PLAYING:
+				countTime -= Time.deltaTime;
+				if(countTime < 0.0f) {
+					countTime = 0.0f;
+					gaameRule.toFlowFail();
+				}
+				leftTimeText.text = countTime.ToString("F2");
+				break;
+
+			default:
+				break;
+		}
+	}
+}
